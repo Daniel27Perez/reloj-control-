fac92d44791667d495b6666bfc80f7e5f03a5bca
Daniel Perez <dan.perezt@duocuc.cl>
2022-06-21 03:52:30 -0400

    primera parte con html django

--- relojcontrol/models.py
+++ relojcontrol/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+from pickle import FALSE, TRUE
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
 
-class Entrada(models.model):
-    titulo = models.CharField(verbose_name= 'Entrada')
+class Entrada(models.Model):
+    titulo = models.DateTimeField(("Ingreso"), default=datetime.now, editable=False)
--- relojcontrol/views.py
+++ relojcontrol/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ingreso(request):
+    return render(request, 'relojcontrol/ingreso.html', {})
