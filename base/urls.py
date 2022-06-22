9211d4e4485469dd8a111e27bf3c092dcf37e8e2
Daniel Perez <dan.perezt@duocuc.cl>
2022-06-22 10:52:30 -0400

    actualizacion

--- base/urls.py
+++ base/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relojcontrol.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
+   
 ]
