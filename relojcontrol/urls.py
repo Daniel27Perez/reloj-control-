05d75b37fcc8fd5bc0839fbec16c6b91c1b79b43
Paolo Alejandro Zuñiga Molina <101225576+PaoloZuniga@users.noreply.github.com>
2022-07-11 23:43:14 -0400

    Update urls.py

--- relojcontrol/urls.py
+++ relojcontrol/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, re_path
 from . import views
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LoginView, logout_then_login, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('ingreso', views.ingreso, name= 'ingreso'),
@@ -16,10 +18,14 @@ urlpatterns = [
     path('asistencia', views.Asistencia, name='asistencia'),
     
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),name="reset_password"),
-    path('password_reset_sent', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'),name="password_reset_done"),
-    re_path('reset/(?P<uidb64>[0-9A-za-z_\-]+)/(?P<token>.+)/$', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirms.html'), name = "password_reset_confirm"),
-    path('reset_password_complete',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_do.html') , name = "password_reset_complete"),
+
+
+
+    path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='index.html', success_url=reverse_lazy('')), name = 'login'),
+    path('reset/password_reset', PasswordResetView.as_view(template_name='registration/password_reset_forms.html', email_template_name="registration/password_reset_email.html"), name = 'password_reset'),
+    path('reset/password_reset_done', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name = 'password_reset_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-za-z_\-]+)/(?P<token>.+)/$', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirms.html'), name = 'password_reset_confirm'),
+    path('reset/done',PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html') , name = 'password_reset_complete'),
    
 ]
-
